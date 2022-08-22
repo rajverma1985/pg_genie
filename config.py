270d814383b5072b786ac9d3fe979da24eeb517b
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-08-22 11:07:09 +0530

    flask config in progress

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
@@ -17,24 +17,23 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['admins@pg_genie.com']
     LANGUAGES = ['en', 'es']
-    POSTS_PER_PAGE = 25
 
 
-# class DevelopmentConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "pg_genie_dev.sqlite")
-#     IMAGE_UPLOADS = os.path.join(basedir, "uploads")
-#
-#
-# class TestingConfig(Config):
-#     TESTING = True
-#     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "pg_genie_test.sqlite")
-#     IMAGE_UPLOADS = os.path.join(basedir, "uploads")
-#
-#
-# class ProductionConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("FLASK_DB_URI") or \
-#                               "sqlite:///" + os.path.join(basedir, "pg_genie_prod.sqlite")
-#     IMAGE_UPLOADS = os.environ.get("FLASK_UPLOADS_FOLDER_URL") or \
-#                     os.path.join(basedir, "uploads")
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "postgresql:///" + os.path.join(basedir, "pg_genie_dev")
+    IMAGE_UPLOADS = os.path.join(basedir, "uploads")
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "pg_genie_test")
+    IMAGE_UPLOADS = os.path.join(basedir, "uploads")
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("FLASK_DB_URI") or \
+                              "sqlite:///" + os.path.join(basedir, "pg_genie_prod")
+    IMAGE_UPLOADS = os.environ.get("FLASK_UPLOADS_FOLDER_URL") or \
+                    os.path.join(basedir, "uploads")
