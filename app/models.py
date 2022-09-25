1c8ecee8e85a7ffe782835bed534017a76e4b9d4
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-09-25 10:52:50 +0530

    Database modelling in progress

--- app/models.py
+++ app/models.py
@@ -5,6 +5,16 @@ from app import db
 class LoginData(UserMixin, db.Model):
     __tablename__ = "login_data"
     id = db.Column(db.Integer(20), primary_key=True, nullable=False)
-    name = db.Column(db.String(20), 'name', nullable=False)
+    login = db.Column(db.String(20), 'login', nullable=False)
     login_time = db.Column(db.DateTime, 'login', nullable=False)
     logout_time = db.Column(db.DateTime, 'logout', nullable=False)
+
+
+class Registration(UserMixin, db.Model):
+    __tablename__ = "Users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(100))
+
+
