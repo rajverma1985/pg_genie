abc2004116ad011979ae1b13674d12c27c6e4ee3
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-11-08 22:56:53 +0530

    main views added

--- app/sandbox.py
+++ app/sandbox.py
@@ -1,9 +1,1 @@
-import os
-from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
-load_dotenv()
-
-url = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
-print('sqlite:/' + os.path.join(basedir, 'app.db'))
