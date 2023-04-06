26f0d0c7c7d1449948d8efa84bb1b0dcde97e10c
Raj Verma <rajverma@Rajs-MacBook-Pro.local>
2023-04-07 00:15:33 +0200

    removed message and notification

--- genie.py
+++ genie.py
@@ -1,10 +1,10 @@
 from app import create_app, db
-from app.models import User, Message, Notification, Task
-
-app = create_app()
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Message': Message,
-            'Notification': Notification, 'Task': Task}
+# from app.models import User, Message, Notification, Task
+#
+# app = create_app()
+#
+#
+# @app.shell_context_processor
+# def make_shell_context():
+#     return {'db': db, 'User': User, 'Message': Message,
+#             'Notification': Notification, 'Task': Task}
